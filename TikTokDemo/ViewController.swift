794861a27d50077a24bf4cc410db89e4f2c4c69e
QuentinZang <quentinzang@outlook.com>
2020-02-22 11:26:59 +0800

    Initial Commit

---
+++ TikTokDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTokDemo
+//
+//  Created by Quentin Zang on 2020/2/22.
+//  Copyright © 2020 臧乾坤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
