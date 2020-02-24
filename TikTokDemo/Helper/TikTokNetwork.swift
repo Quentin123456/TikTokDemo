2e908f1b1057454ac9d471a28155e22dddb8e6a5
QuentinZang <quentinzang@outlook.com>
2020-02-24 15:01:26 +0800

    封装网络请求

--- TikTokDemo/Helper/TikTokNetwork.swift
+++ TikTokDemo/Helper/TikTokNetwork.swift
@@ -7,9 +7,34 @@
 //
 
 import Foundation
+import Just
 
 struct TiktokURL {
     static let baseLocal = "http://127.0.0.1:5500/"
     static let feedFile = "feed.json"
     
+    static let commentLocal = URL(string: baseLocal + "/comment/xsy.json")!
+}
+
+typealias Success = () -> Void
+typealias SuccessWithJson = (Any?) -> Void
+
+struct TiktokNetwork {
+    static let ERR = "json获取错误："
+    
+    //获取本地评论的json
+    static func getComment(success: @escaping SuccessWithJson) {
+        TiktokNetwork.getJson(url: TiktokURL.commentLocal, success: success)
+    }
+    
+    
+    static func getJson(url: URL, success: @escaping SuccessWithJson) {
+        Just.get(url) { (r) in
+            if r.ok {
+                success(r.content)
+            } else {
+                debugPrint(TiktokNetwork.ERR, r.reason)
+            }
+        }
+    }
 }
